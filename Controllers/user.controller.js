8785ea4ae0d0eff43d617cac1b85913f4c242991
ykkim97 <97ykkim@naver.com>
2024-06-08 23:25:50 +0800

    user login info 저장 처리

---
+++ Controllers/user.controller.js
@@ -0,0 +1,24 @@
+const User = require("../Models/user");
+
+const userController = {};
+
+userController.saveUser = async (userName, socketId) => {
+    // --이미 존재하는 유저인지 확인 처리--
+    let user = await User.findOne({ name : userName });
+
+    // 존재하지 않다면 새로 생성
+    if (!user) {
+        user = new User({ 
+            name : userName,
+            token : socketId,
+            online : true,
+        })
+    }
+    // 이미 있는 유저라면 연결정보 token값만 바꾸기 (token이 socketId가 되기 때문)
+    user.token = socketId;
+    user.online = true;
+
+    await user.save();
+    return user;
+}
+module.exports = userController;
--- utils/io.js
+++ utils/io.js
@@ -1,9 +1,21 @@
+const userController = require("../Controllers/user.controller");
+
 module.exports = function(io) {
     // io ~~~~
 
     // on() => 듣기, emit() => 말하기
     io.on("connection", async (socket) => {
-        console.log("client is connected...", socket.id)
+        console.log("client is connected...", socket.id);
+
+        socket.on("login", async (userName, callback) => {
+            // saveUser()로 userName과 socketId를 매개변수로 전달하여 유저정보 저장
+            try { 
+                const user = await userController.saveUser(userName, socket.id);
+                callback({ ok: true, data:user });
+            } catch(error) {
+                callback({ ok: false, error: error.message });
+            }
+        })
 
         // 브라우저를 닫아서 연결이 끊기면 아래 내용 실행됨
         socket.on("disconnect", () => {
